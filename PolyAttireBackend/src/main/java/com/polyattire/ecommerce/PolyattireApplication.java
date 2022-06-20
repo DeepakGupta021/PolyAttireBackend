69c05e2dbd39c933159558ddafcdecf3842045b1
Deepak <itsme.deepakgupta97@gmail.com>
2022-06-20 21:24:27 +0530

    Implemented spring security stage 1
    create access and refresh tokens,
    login via user emails and passwords.

--- PolyAttireBackend/src/main/java/com/polyattire/ecommerce/PolyattireApplication.java
+++ PolyAttireBackend/src/main/java/com/polyattire/ecommerce/PolyattireApplication.java
@@ -1,14 +1,46 @@
 package com.polyattire.ecommerce;
 
+import java.util.ArrayList;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+import com.polyattire.ecommerce.entity.EcomRole;
+import com.polyattire.ecommerce.entity.EcomUser;
+import com.polyattire.ecommerce.service.UserService;
+
+@SpringBootApplication
 public class PolyattireApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolyattireApplication.class, args);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncode() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Bean
+	CommandLineRunner run(UserService userService) {
+		return args ->{
+			userService.saveRole(new EcomRole(null,"ROLE_USER"));
+			userService.saveRole(new EcomRole(null,"ROLE_MANAGER"));
+			userService.saveRole(new EcomRole(null,"ROLE_ADMIN"));
+			userService.saveRole(new EcomRole(null,"ROLE_SUPER_ADMIN"));
+			
+			userService.saveUser(new EcomUser(null,"jack","jack@gmail.com","1234", new ArrayList<EcomRole>()));
+			userService.saveUser(new EcomUser(null,"dummy","dumy@gmail.com","1234", new ArrayList<EcomRole>()));
+			
+			userService.attachRoleToUser("jack@gmail.com", "ROLE_ADMIN");
+			userService.attachRoleToUser("jack@gmail.com", "ROLE_USER");
+			userService.attachRoleToUser("dumy@gmail.com", "ROLE_USER");
+		};
 	}
 
 }
