de457ea2c4ebd6ae630cb3fad5b81c6386290572
Deepak <37866311+flyingd007@users.noreply.github.com>
2022-06-20 20:44:47 +0530

    Revert "Revert "Implemented spring security stage 1""
    This reverts commit 2e438a40020f3acd7611f5088eaaf95328753867.

--- PolyAttireBackend/src/main/java/com/polyattire/ecommerce/service/UserServiceImpl.java
+++ PolyAttireBackend/src/main/java/com/polyattire/ecommerce/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package com.polyattire.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.polyattire.ecommerce.dao.EcomRoleRepo;
@@ -12,12 +20,14 @@ import com.polyattire.ecommerce.dao.EcomUserRepo;
 import com.polyattire.ecommerce.entity.EcomRole;
 import com.polyattire.ecommerce.entity.EcomUser;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Transactional
 @Slf4j
-public class UserServiceImpl implements UserService{
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService, UserDetailsService{
 	
 	@Autowired
 	private EcomUserRepo userRepo;
@@ -25,21 +35,61 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private EcomRoleRepo roleRepo;
 	
+	@Autowired
+	private final PasswordEncoder passwordEncoder;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		EcomUser user =  userRepo.findByEmail(email);
+		
+		if(user==null) {
+			log.error("User not found in the database");
+			throw new UsernameNotFoundException("User not found in the database");
+		}else {
+			log.info("User found in the database {}",email);
+		}
+		Collection<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
+		
+		user.getRoles().forEach(role->{
+			authorities.add(new SimpleGrantedAuthority(role.getName()));
+		});
+		
+		return new User(user.getEmail(),user.getPassword(),authorities);
+	}
+	
 	@Override
 	public EcomUser saveUser(EcomUser user) {
 		log.info("saving new user {} to the database",user.getEmail());
+		
+		if(userRepo.findByEmail(user.getEmail())!=null) {
+			log.info("user {} with given email already exists",user.getEmail());
+			return null;
+		}
+		
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
 		return userRepo.save(user);
 	}
 
 	@Override
 	public EcomRole saveRole(EcomRole role) {
+		
+		if(roleRepo.findByName(role.getName())!=null) {
+			log.info("role {} with given role name already exists",role.getName());
+			return null;
+		}
+		
 		return roleRepo.save(role);
 	}
 
 	@Override
 	public void attachRoleToUser(String username, String roleName) {
 		EcomUser user = userRepo.findByEmail(username);
-		user.getRoles().add(roleRepo.findByName(roleName));
+		
+		if(!user.getRoles().contains(roleRepo.findByName(roleName)))
+		{
+			user.getRoles().add(roleRepo.findByName(roleName));
+		}
+		
 		
 	}
 
@@ -52,5 +102,7 @@ public class UserServiceImpl implements UserService{
 	public List<EcomUser> getUser() {
 		return userRepo.findAll();//implement paging
 	}
+
+	
 
 }
