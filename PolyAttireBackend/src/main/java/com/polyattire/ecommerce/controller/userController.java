5a3c2c4e9d3d9cc0de4ecfa885f7c3027f1877f1
Deepak <37866311+flyingd007@users.noreply.github.com>
2022-06-20 20:42:53 +0530

    Implemented spring security stage 1
    create access and refresh tokens,
    login via user emails and passwords.

--- PolyAttireBackend/src/main/java/com/polyattire/ecommerce/controller/userController.java
+++ PolyAttireBackend/src/main/java/com/polyattire/ecommerce/controller/userController.java
@@ -1,5 +1,6 @@
 package com.polyattire.ecommerce.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.polyattire.ecommerce.entity.EcomRole;
 import com.polyattire.ecommerce.entity.EcomUser;
 import com.polyattire.ecommerce.service.UserService;
+
+import lombok.Data;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
@@ -30,12 +34,26 @@ public class userController {
 	@PostMapping(path = "/api/user/save",consumes = "application/json", produces = "application/json")
 	public ResponseEntity<EcomUser> saveUser(@RequestBody EcomUser ecomUser)
 	{
-		return new ResponseEntity<EcomUser>(service.saveUser(ecomUser),null,201);
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+		return ResponseEntity.created(uri).body(service.saveUser(ecomUser));	
 	}
 	
 	@PostMapping(path = "/api/role/save",consumes = "application/json", produces = "application/json")
 	public ResponseEntity<EcomRole> saveRole(@RequestBody EcomRole ecomRole)
 	{
-		return new ResponseEntity<EcomRole>(service.saveRole(ecomRole),null,201);
+		URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/role/save").toUriString());
+		return ResponseEntity.created(uri).body(service.saveRole(ecomRole));
+	}
+	
+	@PostMapping(path = "/role/addroletouser",consumes = "application/json")
+	public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form){
+		service.attachRoleToUser(form.getUsername(), form.getRoleName());
+		return ResponseEntity.ok().build();
+	}
+	
+	@Data
+	class RoleToUserForm{
+		private String username;
+		private String roleName;
 	}
 }
